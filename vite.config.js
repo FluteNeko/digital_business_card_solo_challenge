ffc21dfabb1c07d3194b5ce876a470743574b953
Flute Neko <jfbm.huesos.github.acc@gmail.com>
2024-06-02 06:40:03 +0100

    deploy with gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/digital_business_card_solo_challenge/",
-    plugins: [react()],
+    plugins: [react()]
 })
