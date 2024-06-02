657687f82048725d4865ac4bfacdc30a79c5824a
Flute Neko <jfbm.huesos.github.acc@gmail.com>
2024-06-02 06:36:50 +0100

    fixing vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/digital_business_card_solo_challenge/",
     plugins: [react()],
 })
